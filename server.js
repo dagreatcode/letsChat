26ba87f8fc5a2ca101839d131e3708d77bb1e103
dagreatcode <dagreatcode@gmail.com>
2021-03-24 17:27:55 -0400

    socket.io

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const http = require("http");
+const app = express();
+const server = http.createServer(app);
+const socket = require("socket.io");
+const io = socket(server);
+const options = { /* ... */ };
+
+
+io.on("connection", socket => {
+    socket.emit("your id", socket.id);
+    socket.on("send message", body => {
+        io.emit("message", body)
+    })
+    console.log('made connection')
+})
+
+
+server.listen(8000, () => console.log("server is running on port 8000"));
