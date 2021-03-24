01ab591b8aae4b818f45b111fe91f9892987abf8
dagreatcode <dagreatcode@gmail.com>
2021-03-24 17:57:50 -0400

    socket.io

--- server.js
+++ server.js
@@ -1,19 +1,30 @@
+require("dotenv").config();
 const express = require("express");
 const http = require("http");
 const app = express();
 const server = http.createServer(app);
 const socket = require("socket.io");
 const io = socket(server);
-const options = { /* ... */ };
+const options = {
+  /* ... */
+};
+const PORT = process.env.PORT || 8000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-io.on("connection", socket => {
-    socket.emit("your id", socket.id);
-    socket.on("send message", body => {
-        io.emit("message", body)
-    })
-    console.log('made connection')
-})
+app.use(express.static("client/build"));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.json(__dirname, "./client/build/index.html"));
+});
 
-server.listen(8000, () => console.log("server is running on port 8000"));
+io.on("connection", (socket) => {
+  socket.emit("your id", socket.id);
+  socket.on("send message", (body) => {
+    io.emit("message", body);
+  });
+  console.log("made connection");
+});
+
+server.listen(PORT, () => console.log("server is running on port 8000"));
